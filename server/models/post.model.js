ac59c18a138bc480198c18af82bc0b83044fc653
Justin Dennison <justin1dennison@gmail.com>
2016-11-18 19:38:48 -0500

    created the post model

---
+++ server/models/post.model.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  cover: {
+    type: String
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String, //this type will change to user object
+    required: true
+  },
+  postDate: {
+    type: Date,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  }
+});
+
+
+var Post = mongoose.model('Post', postSchema);
+module.exports = Post;
