8ea7220ebd7919fc2dc69e05deec8006e30a4365
Justin Dennison <justin1dennison@gmail.com>
2016-11-18 19:58:06 -0500

    added connection to mongo deployment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var server = express();
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 8080;
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
 
+mongoose.connect(mongoURI); //establish the connection to the mongo database
 
 server.get('/', function(req, res){
   res.send('I am working!');
