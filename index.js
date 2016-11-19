e517cbdea9698d825922d1f33b1afbf79e9ce0f7
Justin Dennison <justin1dennison@gmail.com>
2016-11-18 20:19:26 -0500

    implement get all posts route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var postRouter = require('./server/routers/post.router.js');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
@@ -10,7 +11,7 @@ mongoose.connect(mongoURI); //establish the connection to the mongo database
 server.get('/', function(req, res){
   res.send('I am working!');
 });
-
+server.use(postRouter);
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -2,7 +2,18 @@ var express = require('express');
 var router = express.Router();
 var Post = require('../models/post.model.js');
 
-router.get('/posts', function(req, res){});
+router.get('/posts', function(req, res){
+  Post.find({}, function(err, posts){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    return res.status(200).json({
+      posts: posts
+    });
+  });
+});
 router.get('/posts/:id', function(req, res){});
 router.post('/posts', function(req, res){});
 router.put('/posts/:id', function(req, res){});
