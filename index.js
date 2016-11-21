132a741b899d4bd39ffe372932a6cef81a3804c4
Justin Dennison <justin1dennison@gmail.com>
2016-11-21 18:21:00 -0500

    implement the post route for blog posts endpoint

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
 var postRouter = require('./server/routers/post.router.js');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect(mongoURI); //establish the connection to the mongo database
 
--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -15,7 +15,21 @@ router.get('/posts', function(req, res){
   });
 });
 router.get('/posts/:id', function(req, res){});
-router.post('/posts', function(req, res){});
+router.post('/posts', function(req, res){
+  var post = new Post(req.body);
+  post.postDate = new Date();
+  post.summary = req.body.body.slice(0, 100) + '...';
+  post.save(function(err){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    return res.status(201).json({
+      msg: 'Success!'
+    });
+  });
+});
 router.put('/posts/:id', function(req, res){});
 router.delete('/posts/:id', function(req, res){});
 
