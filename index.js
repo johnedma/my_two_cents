999b943a0111db2118221d384b26dbf574e517ce
johned <johned.matt@gmail.com>
2016-11-21 18:21:41 -0500

    implemented the post route for blog posts endpoints

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
 var postRouter = require('./server/routers/post.router.js'); //links our postrouter to indexjs
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true})); //body parser will be used in multiplaces (post and user router) so we set up in index and it used to parse info from mongoose so should start before
 
 mongoose.connect(mongoURI); //establish the connection to the mongodatabase
 
