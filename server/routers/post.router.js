5f0a7ba0851832713a94f815b3bfb4c61da251f5
johned <johned.matt@gmail.com>
2016-11-21 19:09:29 -0500

    implemented put and delete routes

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -14,11 +14,19 @@ router.get('/posts', function(req, res){
     });
   });
 });
-router.get('/posts/id', function(req, res){});
+router.get('/posts/id', function(req, res){
+  post.find({_id: req.params.id}, function(err, post){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+  });
+});
 router.post('/posts', function(req, res){
   var post = new Post(req.body);
   post.postDate = new Date();
-  post.summary = req.body.body.slice(0, 100) + '...';
+  post.summary = req.body.body.slice(0, 100) + '...'; //requests json body and the bodyy attribute within it
   post.save(function(err){
     if(err){
       return res.status(500).json({
@@ -30,7 +38,18 @@ router.post('/posts', function(req, res){
     });
   });
 });
-router.put('/posts/:id', function(req, res){});
+router.put('/posts/:id', function(req, res){ //ask why this is like this
+  Post.findOneAndUpdate({_id: req.params.id}, req.body, function(err, post){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    return res.status(200).json({
+      msg: oldPost
+    });
+  });
+});
 router.delete('/posts/:id', function(req, res){});
 
 
