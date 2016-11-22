b2196fe525806fa05203ef2dabef0edf15c1c16b
Justin Dennison <justin1dennison@gmail.com>
2016-11-21 19:05:33 -0500

    created the delete route

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -53,7 +53,18 @@ router.put('/posts/:id', function(req, res){
     });
   })
 });
-router.delete('/posts/:id', function(req, res){});
+router.delete('/posts/:id', function(req, res){
+  Post.findOneAndRemove({_id: req.params.id}, function(err, deletedPost){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      })
+    }
+    return res.status(200).json({
+      msg: deletedPost
+    });
+  });
+});
 
 
 module.exports = router;
