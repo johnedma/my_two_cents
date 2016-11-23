76a7f7e98be6aceefa20ea976233a3e3981fbe89
johned <johned.matt@gmail.com>
2016-11-22 19:22:42 -0500

    added delete function

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -7,6 +7,7 @@
   function MainController($scope, PostService){
     $scope.posts = PostService.getAll();
     $scope.create = create;
+    $scope.delete = deleteOne; //cant be called just id one internal one dom
 
     $scope.$watch(function(){
       return PostService.getAll();
@@ -16,6 +17,10 @@
     function create(newPost){
       PostService.create(newPost);
       $scope.newPost = {}; //clear the form fields when the function runs
+    }
+    function deleteOne(id){
+      PostService.delte(id);
+      $scope.deleteID = '';
     }
   }
 
--- public/js/services/post.service.js
+++ public/js/services/post.service.js
@@ -39,6 +39,14 @@
           });
     }
     function update(id, newPostData){}
-    function deleteOne(id){}
+    function deleteOne(id){
+      $http.delete(baseUrl + id)
+        .then(function(response){
+          init();
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+    }
   }
 }());
