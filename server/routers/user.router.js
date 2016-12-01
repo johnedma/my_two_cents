b1181afe1fa76f4f9efd386e16b17172051085ab
johned <johned.matt@gmail.com>
2016-11-30 20:36:52 -0500

    implemented login route'

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -34,7 +34,34 @@ router.post('/users/signup', function(req, res){
     });
   });
 });
-router.post('/users/login', function(req, res){});
+router.post('/users/login', function(req, res){ //user has to thru all this validation before being submitted a token
+  //edge case missing information on the request [extra safety method if the form doesnt catch for sometype of reason or they're using an old browser]
+  if(!req.body.email || !req.body.password){
+    return res.status(401).json({
+      msg: "The username or password you have provided is incorrect"
+    }); //be as ambiguous as possible in regards to login errors, say both but not just "right username, wrong pw"
+  }
+  User.findOne({email: req.body.email}, function(err, user){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    if(!user){
+      return res.status(401).json({
+        msg: "The username or password you have provided is incorrect"
+      });
+    }
+    if(!user.validPassword(req.body.password)){
+      return res.status(401).json({
+        msg: "The username or password you have provided is incorrect"
+      });
+    }
+    return res.status(200).json({
+      token: user.generateJwt()
+    });
+  }); //safe to findOne by email because we set as unique so there cant be two uses with the same email
+});
 router.put('/users/profile/:userId', function(req, res){});
 
 module.exports = router;
